ccbbc1cb4a6aa0aa7c3f7a780ac1b2f5f0815dcc
Evgenus <chernyshov.eugene@gmail.com>
2014-08-09 04:18:15 +0300

    RSA test

--- forge/forge-tests.ts
+++ forge/forge-tests.ts
@@ -85,3 +85,116 @@ function testRC2(someBytes: string) {
     // outputs decrypted hex
     console.log(cipher.output.toHex());
 }
+
+function testRSA() {
+    var rsa = forge.pki.rsa;
+
+    // generate an RSA key pair synchronously
+    var keypair = rsa.generateKeyPair({ bits: 2048, e: 0x10001 });
+
+    // generate an RSA key pair asynchronously (uses web workers if available)
+    // use workers: -1 to run a fast core estimator to optimize # of workers
+    rsa.generateKeyPair({ bits: 2048, workers: 2 }, function (err, keypair) {
+        // keypair.privateKey, keypair.publicKey
+    });
+
+    // generate an RSA key pair in steps that attempt to run for a specified period
+    // of time on the main JS thread
+    var state = rsa.createKeyPairGenerationState(2048, 0x10001);
+    var step = function () {
+        // run for 100 ms
+        if (!rsa.stepKeyPairGenerationState(state, 100)) {
+            setTimeout(step, 1);
+        }
+        else {
+            // done, turn off progress indicator, use state.keys
+        }
+    };
+    // turn on progress indicator, schedule generation to run
+    setTimeout(step);
+
+    var privateKey = keypair.privateKey;
+    var publicKey = keypair.publicKey;
+
+    // sign data with a private key and output DigestInfo DER-encoded bytes
+    // (defaults to RSASSA PKCS#1 v1.5)
+    var md = forge.md.sha1.create();
+    md.update('sign this', 'utf8');
+    var signature = privateKey.sign(md);
+
+    // verify data with a public key
+    // (defaults to RSASSA PKCS#1 v1.5)
+    var verified = publicKey.verify(md.digest().bytes(), signature);
+
+    // sign data using RSASSA-PSS where PSS uses a SHA-1 hash, a SHA-1 based
+    // masking function MGF1, and a 20 byte salt
+    var md = forge.md.sha1.create();
+    md.update('sign this', 'utf8');
+    var pss = forge.pss.create({
+        md: forge.md.sha1.create(),
+        mgf: forge.mgf.mgf1.create(forge.md.sha1.create()),
+        saltLength: 20
+        // optionally pass 'prng' with a custom PRNG implementation
+        // optionalls pass 'salt' with a forge.util.ByteBuffer w/custom salt
+    });
+    var signature = privateKey.sign(md, pss);
+
+    // verify RSASSA-PSS signature
+    var pss = forge.pss.create({
+        md: forge.md.sha1.create(),
+        mgf: forge.mgf.mgf1.create(forge.md.sha1.create()),
+        saltLength: 20
+        // optionally pass 'prng' with a custom PRNG implementation
+    });
+    var md = forge.md.sha1.create();
+    md.update('sign this', 'utf8');
+    publicKey.verify(md.digest().getBytes(), signature, pss);
+
+    var bytes = "bytes";
+
+    // encrypt data with a public key (defaults to RSAES PKCS#1 v1.5)
+    var encrypted = publicKey.encrypt(bytes);
+
+    // decrypt data with a private key (defaults to RSAES PKCS#1 v1.5)
+    var decrypted = privateKey.decrypt(encrypted);
+
+    // encrypt data with a public key using RSAES PKCS#1 v1.5
+    var encrypted = publicKey.encrypt(bytes, 'RSAES-PKCS1-V1_5');
+
+    // decrypt data with a private key using RSAES PKCS#1 v1.5
+    var decrypted = privateKey.decrypt(encrypted, 'RSAES-PKCS1-V1_5');
+
+    // encrypt data with a public key using RSAES-OAEP
+    var encrypted = publicKey.encrypt(bytes, 'RSA-OAEP');
+
+    // decrypt data with a private key using RSAES-OAEP
+    var decrypted = privateKey.decrypt(encrypted, 'RSA-OAEP');
+
+    // encrypt data with a public key using RSAES-OAEP/SHA-256
+    var encrypted = publicKey.encrypt(bytes, 'RSA-OAEP', {
+        md: forge.md.sha256.create()
+    });
+
+    // decrypt data with a private key using RSAES-OAEP/SHA-256
+    var decrypted = privateKey.decrypt(encrypted, 'RSA-OAEP', {
+        md: forge.md.sha256.create()
+    });
+
+    // encrypt data with a public key using RSAES-OAEP/SHA-256/MGF1-SHA-1
+    // compatible with Java's RSA/ECB/OAEPWithSHA-256AndMGF1Padding
+    var encrypted = publicKey.encrypt(bytes, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
+
+    // decrypt data with a private key using RSAES-OAEP/SHA-256/MGF1-SHA-1
+    // compatible with Java's RSA/ECB/OAEPWithSHA-256AndMGF1Padding
+    var decrypted = privateKey.decrypt(encrypted, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
+}
